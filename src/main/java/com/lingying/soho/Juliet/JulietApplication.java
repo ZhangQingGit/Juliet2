ba8e590e2b1ba15507a0c84650e65129f555119f
ZhangQing <@DESKTOP-HMLA68C>
2019-09-09 08:57:35 +0800

    Juliet 功能版实现

---
+++ src/main/java/com/lingying/soho/Juliet/JulietApplication.java
@@ -0,0 +1,13 @@
+package com.lingying.soho.Juliet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingying/soho/Juliet/JulietApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lingying.soho.Juliet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JulietApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
