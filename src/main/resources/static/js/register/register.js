685971c99bba5cb8dc6d8975013a81659b57c6b3
CaoPengHui <1282182510@qq.com>
2019-09-17 15:49:50 +0800

    登陆注册的进一步修改，与后端的交互

--- src/main/resources/static/js/register/register.js
+++ src/main/resources/static/js/register/register.js
@@ -1,7 +1,34 @@
-/**
- * 
- */
+layui.use('form', function(){
+	var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
+
+	//……
+
+	//但是，如果你的HTML是动态生成的，自动渲染就会失效
+	//因此你需要在相应的地方，执行下述方法来进行渲染
+	form.render();
+});
+
+function emailRegisterLi() {
+	//$(this).addClass("selected").siblings().removeClass("selected");
+	$('#register-email-form').show();
+	$('#register-phone-form').hide();
+}
+
+function phonelRegisterLi() {
+
+	$('#register-email-form').hide();
+	$('#register-phone-form').show();
+}
+
 
 function register() {
-	alert("我要注册!");
+	alert("adadsd");
+}
+
+function emailRegister() {
+	var useremail=$('#useremail').val();
+	alert(useremail);
+	$.post("email",{useremail:useremail},function(data,statu){
+
+	});
 }
