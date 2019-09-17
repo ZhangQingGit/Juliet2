a04f46fb77e694f147ed70632f1401d4830ecbb4
ZhangQing <@DESKTOP-HMLA68C>
2019-09-17 09:40:55 +0800

    静态资源整合

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -5,3 +5,13 @@
 function login() {
 	alert("我进来了!");
 }
+
+layui.use('form', function(){
+	var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
+
+	//……
+
+	//但是，如果你的HTML是动态生成的，自动渲染就会失效
+	//因此你需要在相应的地方，执行下述方法来进行渲染
+	form.render();
+});
