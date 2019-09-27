3a38be118461cc47896fa21fe6e58722a33432c2
ZhangQing <643164399@qq.com>
2019-09-27 16:40:51 +0800

    搜索页面上方选项框

--- src/main/resources/static/js/tasklist/tasklist.js
+++ src/main/resources/static/js/tasklist/tasklist.js
@@ -2,8 +2,24 @@ $(document).ready(function(){
     $.post("getName", function (data) {
         $("#d_name").append(data.message);
     });
+
+    $.post("showList", function (data, staus) {
+        for (var i = 0; i < data.length; i++) {
+            $('#filterCollapse').append('<li class="multi-chosen" id="father'+i+'"><span class="title">'+data[i].father+'：</span>\n' +
+                '        <a rel="nofollow" href="javascript:;" class="active"\n' +
+                '        data-lg-tj-cid="idnull">不限\n' +
+                '        </a>'+
+                '          </li>');
+            var str = data[i].son.split(",");
+            for(var j = 0; j < str.length; j++){
+                $('#father'+i).append('<a rel="nofollow" href="javascript:;" data-lg-tj-id=8r00 data-lg-tj-no="0002"\n' +
+                    '        data-lg-tj-cid="idnull">'+str[j]+'\n' +
+                    '        <i class="delete"></i>');
+            }
+        }
+    });
+
     $.post("/task/taskList",function (json) {
-        console.log(json);
         for(var i=0;i<json.data.length;i++){
             $("#taskList").append('<li class="con_list_item default_list">\n' +
                 '                        <span class="top_icon direct_recruitment"></span>\n' +
