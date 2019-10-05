072bab3d40a28dc61da6497962ac7ae5e00be921
CaoPengHui <1282182510@qq.com>
2019-10-05 17:33:07 +0800

    完善信息的修改，关闭当前页面代码的编写

--- src/main/resources/static/js/perfect/teamperfect.js
+++ src/main/resources/static/js/perfect/teamperfect.js
@@ -10,7 +10,19 @@ function teamPerfect() {
         experience:experience},function (data,status) {
 
         if(data.state == 200){
-            window.location.href="releasetask.html"
+            if (navigator.userAgent.indexOf('MSIE') > 0) { // close IE
+                if (navigator.userAgent.indexOf('MSIE 6.0') > 0) {
+                    window.opener = null;
+                    window.close();
+                } else {
+                    window.open('', '_top');
+                    window.top.close();
+                }
+            } else { // close chrome;It is effective when it is only one.
+                window.opener = null;
+                window.open('', '_self');
+                window.close();
+            }
         }else {
             alert(data.message);
         }
