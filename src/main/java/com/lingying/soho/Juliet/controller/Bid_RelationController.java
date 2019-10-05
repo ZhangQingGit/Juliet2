98b91d2c6e17ad27b3331a6800e5bb7a95bb3a57
ZhangQing <643164399@qq.com>
2019-10-05 08:54:48 +0800

    给controller加父类，在发布任务时，对任务表达意向时，增加验证

--- src/main/java/com/lingying/soho/Juliet/controller/Bid_RelationController.java
+++ src/main/java/com/lingying/soho/Juliet/controller/Bid_RelationController.java
@@ -3,11 +3,13 @@ package com.lingying.soho.Juliet.controller;
 import com.lingying.soho.Juliet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lingying.soho.Juliet.service.Bid_RelationService;
 import com.lingying.soho.Juliet.util.ResponseResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,29 +20,35 @@ public class Bid_RelationController extends BaseController{
     private Bid_RelationService bidService;
     @Autowired
     private UserService userService;
-    
+
     @RequestMapping("bid_relation")
-    public String addRelation(String tname, String pname, String cname, HttpSession session){
+    @ResponseBody
+    public ResponseResult<String> addRelation(String tname, String pname, String cname, HttpSession session){
         Integer uid = getUidFromSession(session);
-        Boolean isHave = userService.findToTeamExit(uid);
+        boolean isHave = userService.findToTeamExit(uid);
         if(isHave){
-            return "perfect";
+            int row = bidService.biddingPeo(tname, pname, cname);
+            if(row==1) {
+                return new ResponseResult<>(200,"意向已传达！");
+            }else {
+                return new ResponseResult<>(201,"不可重复表明意向！");
+            }
         }
-        int row = bidService.biddingPeo(tname, pname, cname);
-        if(row==1) {
-            return "taskdetails";
-        }else {
-           return "taskdetails";
-        }
+        return null;
     }
     
     @RequestMapping("del_relation")
     @ResponseBody
-    public ResponseResult<String> delRelation(String tname, String pname, String cname){
-       int row = bidService.Delbidding(tname, pname, cname);
-       if(row==1) {
-           return new ResponseResult<>(200,"取消意向成功！");
-       }
-       return new ResponseResult<>(201,"不可重复表明意向！");
+    public ResponseResult<String> delRelation(String tname, String pname, String cname, HttpSession session){
+        Integer uid = getUidFromSession(session);
+        boolean isHave = userService.findToTeamExit(uid);
+        if(isHave){
+            int row = bidService.Delbidding(tname, pname, cname);
+            if(row==1) {
+                return new ResponseResult<>(200,"取消意向成功！");
+            }
+            return new ResponseResult<>(201,"不可重复表明意向！");
+        }
+        return null;
     }
 }
